432358ae44835040d028ce1871bfef790e333ca5
CaioSilva2026 <chferrazsilva2@gmail.com>
2026-08-19 20:00:24 -0300

    api: adicionado rota de metricas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
-//const modbusRoutes = require('./routes/modbus');
+
+const sessoesRoutes = require('./service/sessoes');
+const metricasRoutes = require('./service/metricas');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,17 +13,22 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/health', async(req, res) => {
-    try{
+app.use('/api/sessoes', sessoesRoutes);
+app.use('/api/metricas', metricasRoutes);
+
+app.get('/health', async (req, res) => {
+    try {
         await db.query('SELECT 1');
+
         res.json({
             status: 'ok',
             timestamp: new Date().toISOString(),
             db: 'connected'
         });
-    } catch(error){
+
+    } catch (error) {
         res.status(500).json({
-            status: 'ok',
+            status: 'error',
             timestamp: new Date().toISOString(),
             db: 'disconnected',
             error: error.message
@@ -28,10 +36,7 @@ app.get('/health', async(req, res) => {
     }
 });
 
-//app.use('/api/modbus', modbusRoutes);
-
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
     console.log(`Health check: http://localhost:${PORT}/health`);
-    //console.log(`Simulação modbus: http://localhost:${PORT}/api/modbus/simular/inicio`);
 });
